e2e07ad15550ae42f42980ad1b4919ce5fe37fd0
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2023-09-21 12:50:28 +0300

    prepare markdown file

--- jest.config.js
+++ jest.config.js
@@ -157,7 +157,7 @@ const config = {
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["/node_modules/", "/db.skip.test.js"],
+  // testPathIgnorePatterns: ["/node_modules/"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
